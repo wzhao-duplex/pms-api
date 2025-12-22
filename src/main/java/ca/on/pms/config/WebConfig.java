e5b404f57708412538a00263a1bdd677a4dda18c
Vincent Zhao <wzhao.duplex@gmail.com>
2025-12-22 15:51:32 -0500

    Add CORS

--- src/main/java/ca/on/pms/config/WebConfig.java
+++ src/main/java/ca/on/pms/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                        		"http://localhost:4200",
+                        		"http://pms-ui-frontend.s3-website-us-east-1.amazonaws.com"
+                        	)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
